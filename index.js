14a1d14e852fd6fd43aab5d98f6b9d66939052b8
TrakaMeitene <sandra.jurberga@gmail.com>
2021-06-09 23:27:34 +0300

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function myFunc() {
+    var ball = document.getElementById("ball");
+    var toggle = document.getElementById("toggle");
+    ball.classList.toggle("show");
+    toggle.classList.toggle("color");
+    document.getElementById("price").innerHTML = "199.99";
+    document.getElementById("price1").innerHTML = "249.99";
+    document.getElementById("price2").innerHTML = "399.99";
+} 

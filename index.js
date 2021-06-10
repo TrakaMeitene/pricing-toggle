a54c91c60c3b758b167ffdd71cfb9087004e1d16
TrakaMeitene <sandra.jurberga@gmail.com>
2021-06-10 12:31:47 +0300

    toggle

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 
 function myFunc() {
-    var ball = document.getElementById("ball");
-    var toggle = document.getElementById("toggle");
-    ball.classList.toggle("show");
-    toggle.classList.toggle("color");
-    document.getElementById("price").innerHTML = "199.99";
-    document.getElementById("price1").innerHTML = "249.99";
-    document.getElementById("price2").innerHTML = "399.99";
-} 
+    var ball = document.getElementById("ball1");
+    var toggle = document.getElementById("toggle1");
+    if (document.getElementById("price").innerHTML === "19.99") 
+    
+    { ball.classList.add("show");
+    toggle.classList.add("color");
+        document.getElementById("price").innerHTML = "199.99 ";
+        document.getElementById("price1").innerHTML = " 249.99";
+        document.getElementById("price2").innerHTML = " 399.99";
+      
+    } else {
+        ball.classList.remove("show");
+        document.getElementById("price").innerHTML = "19.99";
+        document.getElementById("price1").innerHTML = "24.99";
+        document.getElementById("price2").innerHTML = "39.99";
+        toggle.classList.remove("color");
+    }
+}
